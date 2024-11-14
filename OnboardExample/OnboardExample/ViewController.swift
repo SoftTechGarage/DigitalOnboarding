f59aec0acd37156245f90be70e6df16815fd6a07
Hakan Yıldızay <hakan.yildizay@softtech.com.tr>
2024-11-14 16:01:47 +0300

    [ST/development] [Hakan Yıldızay] [Version 2.3.7 Fetching Theme]

--- OnboardExample/OnboardExample/ViewController.swift
+++ OnboardExample/OnboardExample/ViewController.swift
@@ -8,36 +8,55 @@ import UIKit
 import onboarding
 import NFCReaderApple
 class ViewController: UIViewController, OnboardingDelegate {
+    
 
+    @IBOutlet weak var deeplinkTextField: UITextView!
     @IBOutlet private weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         label.text = "Version \(CoreReader.getSDKVersion())"
         label.textColor = .red
         Onboarding.delegate = self
         
-        
-        let width: CGFloat = 100
-        let openBtn = UIButton(frame: CGRect(x: (view.frame.width - width) / 2,
-                                             y: view.frame.height / 2,
-                                             width: width,
-                                             height: 40))
-        openBtn.setTitle("Open SDK", for: .normal)
-        openBtn.addTarget(self, action: #selector(openOnboarding), for: .touchUpInside)
-        openBtn.setTitleColor(UIColor.blue, for: .normal)
-        view.addSubview(openBtn)
     }
 
-    @objc func openOnboarding(){
-        
-        Onboarding(vc: self.navigationController!)
-            .setLanguage(language: "tr")
-            .setEndpoint(endPoint: "http://YOUR_ENDPOINT")
-            .setPredefined(predefined: "YOUR_ID")
-            .setExternalKey(externalKey: "YOUR_EXTERNAL_KEY") //optional
-            .start()
-    }
     
+    /**
+     deeplinkText = https://www.trademaster.com.tr/onboarding/?processId={encodedProcessIdstring}
+     */
+    @IBAction func openOnboarding(_ sender: Any) {
+        
+        if let deeplinkText = self.deeplinkTextField.text,
+           !deeplinkText.isEmpty
+        {
+            
+            let urlComponents = URLComponents(string: deeplinkText)
+            let pId = urlComponents?.queryItems?.first(where: { $0.name == "processId" })?.value ?? ""
+            
+            if !pId.isEmpty {
+                Onboarding
+                    .init(vc: self.navigationController)
+                    .setProcess(process: pId)
+                    .start()
+                
+            }
+            
+        } else {
+            
+            Onboarding(vc: self.navigationController!)
+                .setLanguage(language: "tr")
+                .setEndpoint(endPoint: "http://YOUR_ENDPOINT")
+                .setPredefined(predefined: "YOUR_ID")
+                .setExternalKey(externalKey: "YOUR_EXTERNAL_KEY") //optional
+                .start()
+        }
+        
+        
+        
+        
+    }
+        
     func onOnboardingResult(resultCode: onboarding.OnboardingResult, code: String, message: String, processId: String) {
         
     }
