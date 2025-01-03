5df95fc8929e7ccc4c79bcbf05b6cc522f7c1baa
Salih KIR <77065124+SalihKIR@users.noreply.github.com>
2025-01-03 16:58:16 +0300

    [ST][SalihKIR][updated OpenSSL framework for xcode16.1 ]

--- Sources/OpenSSL.xcframework/tvos-arm64/OpenSSL.framework/Headers/ecdh.h
+++ Sources/OpenSSL.xcframework/tvos-arm64/OpenSSL.framework/Headers/ecdh.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2016 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2002-2016 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
@@ -7,4 +7,4 @@
  * https://www.openssl.org/source/license.html
  */
 
-#error "This file is obsolete; please update your software."
+#include <OpenSSL/ec.h>
